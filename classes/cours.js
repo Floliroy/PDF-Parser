420e5fc86d0939edc04a9d558b24978f260becc0
Flo Leeroy <florian.leeroy@hotmail.com>
2020-08-24 23:37:01 +0200

    attempt to convert coord into hour
    fixes needed for hour > 16h
    also there is some "Lieu" that are not at rhier corresponding"Cours"

--- classes/cours.js
+++ classes/cours.js
@@ -1,3 +1,43 @@
+function getClosest(min){
+    minutes = [0, 15, 30, 45, 60]
+
+    closest = 0
+    lastCalc = 60
+    minutes.forEach(function(elem){
+        if(Math.abs(elem - min) < lastCalc){
+            lastCalc = Math.abs(elem - min)
+            closest = elem
+        }
+    })
+    return closest
+}
+
+function secondesToHeure(secondes){
+    let hour  = Math.floor(secondes / 3600)
+    let min = Math.floor((secondes - (hour * 3600)) / 60)
+    
+    min = getClosest(min)
+    if(min == 60){
+        hour++
+        min = 0
+    }
+    if(hour < 10){hour = `0${hour}`}
+    if(min  < 10){min  = `0${min }`}
+
+    return `${hour}:${min}`
+}
+
+function getSecondesByMultiplier(coordX){
+    return  (-0.0000000000000240554) * Math.pow(coordX, 7)
+            + 0.0000000000563162 * Math.pow(coordX, 6)
+            + (-0.000000043126) * Math.pow(coordX, 5)
+            + 0.00000630924 * Math.pow(coordX, 4)
+            + 0.00728924 * Math.pow(coordX, 3)
+            + (-3.69793) * Math.pow(coordX, 2)
+            + 678.77 * coordX - 11216.7
+}
+
+
 module.exports = class Cours{
     #titre = ""
     #prof = ""
@@ -31,6 +71,18 @@ module.exports = class Cours{
         this.#coordY = coordY
         this.#width  = width
         this.#height = height
+    }
+
+    getHeureDebut(){
+        return secondesToHeure(getSecondesByMultiplier(this.#coordX))
+    }
+    getHeureFin(){
+        return secondesToHeure(getSecondesByMultiplier(this.#endOfCase))
+    }
+
+    print(){
+        console.log(`       [${this.getHeureDebut()}${this.getEndOfCase() ? " - " + this.getHeureFin() : ""}]`)
+        console.log(`       ${this.#titre}${this.#lieu ? " - " + this.#lieu : ""}${this.#prof ? " (" + this.#prof + ")" : ""}`)
     }
 
     getTitre(){
@@ -81,7 +133,4 @@ module.exports = class Cours{
         return this.#evaluation
     }
 
-    print(){
-        console.log(`       ${this.#titre}${this.#lieu ? " - " + this.#lieu : ""}${this.#prof ? " (" + this.#prof + ")" : ""}`)
-    }
 }
