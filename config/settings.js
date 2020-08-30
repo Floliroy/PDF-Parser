2a633b396c482bf08bee7a6408d685f8ead9b14e
Flo Leeroy <florian.leeroy@hotmail.com>
2020-08-30 02:39:10 +0200

    remove useless comments

--- config/settings.js
+++ config/settings.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const SERVICE_ACCT_ID = process.env.GOOGLE_EMAIL
 const KEY = process.env.GOOGLE_TOKEN.replace(/\\n/g, '\n')
 const TIMEZONE = "UTC+02:00"
-//const CALENDAR_URL = "https://calendar.google.com/calendar?cid=YTZrY2Z0aW0zbGxpa2Q1cGdsZGxxcWZtY29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
 const CALENDAR_ID = {
 	"primary": "a6kcftim3llikd5pgldlqqfmco@group.calendar.google.com"
 }
@@ -11,5 +10,4 @@ const CALENDAR_ID = {
 module.exports.serviceAcctId = SERVICE_ACCT_ID
 module.exports.key = KEY
 module.exports.timezone = TIMEZONE
-//module.exports.calendarUrl = CALENDAR_URL
 module.exports.calendarId = CALENDAR_ID
