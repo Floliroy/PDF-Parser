4415af1878b32eaf558ab0d478c71e49e67d4f4b
Floliroy <45274794+Floliroy@users.noreply.github.com>
2021-09-09 07:50:08 +0200

    Revert "Revert "module auto detect hour""
    This reverts commit d2940c70489f65b2cfd6a359d6e018056319baee.

--- modules/parseCoord.js
+++ modules/parseCoord.js
@@ -1,7 +1,24 @@
+const hours = ["8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h"]
+let coords = new Map()
+
 module.exports = class ParseCoord{
 
-    static coordToHeure(coordX, addHour){
-        return secondesToHeure(getSecondesByCoord(coordX), addHour)
+    static loadDatas(datas, id){
+        let hourToCoord = new Map()
+        for(const hour of hours){
+            for(const data of datas){
+                if(data.str == hour){
+                    hourToCoord.set(hour, data.x)
+                    break
+                }
+            }
+        }
+        if(hourToCoord.has(id)) hourToCoord.delete(id)
+        coords.set(id, hourToCoord)
+    }
+
+    static coordToHeure(coordX, addHour, id){
+        return secondesToHeure(getSecondesByCoord(coordX, id), addHour)
     }
     
 }
@@ -15,13 +32,13 @@ function getClosest(min){
 
     let closest = 0
     let lastCalc = 60
-    minutes.forEach(function(elem){
+    for(const minute of minutes){
         //On cherche la distance la plus courte avec la minute passée
-        if(Math.abs(elem - min) < lastCalc){
-            lastCalc = Math.abs(elem - min)
-            closest = elem
+        if(Math.abs(minute - min) < lastCalc){
+            lastCalc = Math.abs(minute - min)
+            closest = minute
         }
-    })
+    }
     return closest
 }
 
@@ -50,20 +67,19 @@ function secondesToHeure(secondes, addHour){
  * Permet de transformer une coordonnée en temps en seconde
  * @param {*} coordX La coordonnée à transformer
  */
-function getSecondesByCoord(coordX){
-    //On gère nos exceptions
-    if(coordX > 237-3 && coordX < 242+3){return 35100} //09h45
-    if(coordX > 261-3 && coordX < 261+3){return 36000} //10h00
-    if(coordX > 384-3 && coordX < 389+3){return 43200} //12h00
-    if(coordX > 394-3 && coordX < 394+3){return 44100} //12h15
-    if(coordX > 478-3 && coordX < 478+3){return 48600} //13h30
-    if(coordX > 609-3 && coordX < 614+3){return 55800} //15h30
-    if(coordX > 644-3 && coordX < 644+3){return 56700} //15h45
-    if(coordX > 782-3 /*&& coordX < 782+3*/){return 63900} //17h45
-    
-    //Sinon on utilise une fonction polynomiale de degrès 4
-    return  0.00000019513359 * Math.pow(coordX, 4)
-            + (-0.0003723993) * Math.pow(coordX, 3)
-            + 0.2325723439 * Math.pow(coordX, 2)
-            + 7.6856610283 * coordX + 25028.095126
+function getSecondesByCoord(coordX, id){
+    const hourToCoord = coords.get(id)
+    let prevCoord
+    for(const [hour, coord] of hourToCoord){
+        if(coord > coordX){
+            if(!prevCoord){
+                return 27900
+            }
+            let seconds = (parseInt(hour.replace("h", "")) - 1) * 3600
+            seconds += (coordX - prevCoord) / (coord - prevCoord) * 3600
+            return seconds
+        }
+        prevCoord = coord
+    }
+    return 63900
 }
